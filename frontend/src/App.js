6df902bd6ad78cbda7dea2166d71f6250cb07dad
hayeonn2 <dwd1205@naver.com>
2024-07-09 15:11:41 +0900

    zustand 설치 후 예제 사용해보기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {useEffect} from "react";
 import axios from "axios";
+import { useStore } from './zustand/useStore';
 
 function App(){
   const getApi = async () => {
@@ -10,7 +11,25 @@ function App(){
     getApi();
   }, []);
 
-  return <div>이건 프론트 페이지!!</div>
+  const bears = useStore((state) => state.bears);
+  const increasePopulation = useStore((state) => state.increasePopulation);
+  const removeAllBears = useStore((state) => state.removeAllBears);
+  const updateBears = useStore((state) => state.updateBears);
+
+  // 새로운 곰의 수를 업데이트하기
+  const handleUpdateBears = () => {
+    updateBears(12);
+  }
+
+  return(
+  <div>
+    이건 프론트 페이지!! z
+    <div>{bears} around heare!! </div>
+    <button onClick={increasePopulation}>up!</button>
+    <button onClick={removeAllBears}>reset</button>
+    <button onClick={handleUpdateBears}>updateBears</button>
+  </div>
+  )
 };
 
 export default App;
